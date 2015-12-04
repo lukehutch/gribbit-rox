6e4a669b7df5c87e77f45ce713f3c61c44dd1d16
Luke Hutchison <luke.hutch@gmail.com>
2015-12-04 10:24:56 -0800

    Add HttpServerDemo

---
+++ src/com/flat502/rox/demo/HttpServerDemo.java
@@ -0,0 +1,74 @@
+package com.flat502.rox.demo;
+
+import java.net.InetAddress;
+
+import com.flat502.rox.http.HttpRequestBuffer;
+import com.flat502.rox.server.AsynchronousRequestHandler;
+import com.flat502.rox.server.HttpServer;
+import com.flat502.rox.server.RequestContext;
+import com.flat502.rox.server.Response;
+import com.flat502.rox.server.ResponseChannel;
+
+/**
+ * A demo synchronous server illustrating the {@link com.flat502.rox.server.SynchronousRequestHandler} interface.
+ */
+public class HttpServerDemo implements AsynchronousRequestHandler {
+    @Override
+    public void handleRequest(RequestContext context, ResponseChannel rspChannel) throws Exception {
+        final HttpRequestBuffer req = context.getHttpRequest();
+        String body = new String(req.getContent());
+        System.out.println("Got request " + req.getURI() + " with body:\n" + body);
+        rspChannel.respond(new Response() {
+            @Override
+            public byte[] getContent() {
+                return ("<html><body><p>Got request: " + req.getURI() + "</p></body></html>").getBytes();
+            }
+
+            @Override
+            public String getContentType() {
+                // TODO Auto-generated method stub
+                return "text/html";
+            }
+        });
+    }
+
+    @Override
+    public boolean canHandleURI(String uri) {
+        return true;
+    }
+
+    /**
+     * Start an instance of this demo server.
+     * <p>
+     * The following XML-RPC methods are supported by this server:
+     * <ul>
+     * <li>{@link RMIServerInterface#sum(int[]) rmi.sum}</li>
+     * <li>{@link RMIServerInterface#getDate() rmi.getDate}</li>
+     * <li>{@link RMIServerInterface#getVersionInfo(boolean) rmi.getVersion}</li>
+     * </ul>
+     * 
+     * @param args
+     *            A list of parameters indicating the <code>host/address</code> and <code>port</code> to bind to.
+     *            These default to <code>localhost</code> and <code>8080</code> if not specified.
+     */
+    public static void main(String[] args) {
+        try {
+            String host = "localhost";
+            int port = 8080;
+
+            if (args != null && args.length > 0) {
+                host = args[0];
+                if (args.length > 1) {
+                    port = Integer.parseInt(args[1]);
+                }
+            }
+            System.out.println("Starting server on " + host + ":" + port);
+
+            HttpServer server = new HttpServer(InetAddress.getByName(host), port);
+            server.registerHandler(new HttpServerDemo());
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
